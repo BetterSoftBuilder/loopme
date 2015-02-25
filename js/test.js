d6d70e86c601f3b336c014290b29bb9845c4b68d
BetterSoftBuilder <dmitrij.stupak@privatbank.ua>
2015-02-25 09:30:39 +0200

    update

--- js/test.js
+++ js/test.js
@@ -1,12 +1,27 @@
-var Tasty = (function() {
+var ad = (function() {
         "use strict";
 	return {
-		init:   function(tastyPlace, cover) {
+		init:   function() {
 
 			    },
 
-		getData:   function(first) {
+		getData:   function() {
+                                var request = new XMLHttpRequest();
+                                request.onreadystatechange = function() {
+                                    if (this.readyState == 4 ) {
+                                       if(this.status == 200){
+                                           var adsData = JSON.parse(this.responseText);
+                                           document.getElementsByClassName('main_img')[0].src = adsData.ads[0].data.image_url;
+                                           console.log(this.responseText);
+                                       }
+                                       else {
+                                           console.log(this.responseText);
+                                       }
+                                    }
+                                };
 
+                                request.open("GET", "json/api.json", true);
+                                request.send();
 			    }
-	}
+	};
 }());
