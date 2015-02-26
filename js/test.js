9cd5ebd836a2c43fae4b3833a4fc335a117ecf03
BetterSoftBuilder <dmitrij.stupak@privatbank.ua>
2015-02-26 17:37:01 +0200

    update

--- js/test.js
+++ js/test.js
@@ -6,22 +6,27 @@ var ad = (function() {
 			    },
 
 		getData:   function() {
-                                var request = new XMLHttpRequest();
-                                request.onreadystatechange = function() {
+                                var requestAd = new XMLHttpRequest();
+                                requestAd.onload = function() {
                                     if (this.readyState == 4 ) {
                                        if(this.status == 200){
                                            var adsData = JSON.parse(this.responseText);
-                                           document.getElementsByClassName('main_img')[0].src = adsData.ads[0].data.image_url;
-                                           console.log(this.responseText);
+                                           var ad = document.getElementsByClassName('main_img')[0];
+                                           ad.src = adsData.ads[0].data.image_url;
+                                           ad.onload = function () {
+                                               var successLoad = new XMLHttpRequest();
+                                               successLoad.onload = function() {console.log('ad was loaded');};
+                                               successLoad.open("GET", adsData.session.beacons.inbox_open, true);
+                                               successLoad.send();
+                                           };
                                        }
                                        else {
                                            console.log(this.responseText);
                                        }
                                     }
                                 };
-
-                                request.open("GET", "json/api.json", true);
-                                request.send();
+                                requestAd.open("GET", "json/api.json", true);
+                                requestAd.send();
 			    }
 	};
 }());
