bed3ed0731a1b31d3d4200a1031e2aa754f0b6bb
BetterSoftBuilder <dmitrij.stupak@privatbank.ua>
2015-02-27 17:40:54 +0200

    update

--- js/test.js
+++ js/test.js
@@ -13,11 +13,32 @@ var ad = (function() {
                                            var adsData = JSON.parse(this.responseText);
                                            var ad = document.getElementsByClassName('main_img')[0];
                                            ad.src = adsData.ads[0].data.image_url;
+                                           ad.onclick = function() {
+                                                document.location.href = adsData.ads[0].data.click_url;
+                                            };
                                            ad.onload = function () {
                                                var successLoad = new XMLHttpRequest();
-                                               successLoad.onload = function() {console.log('ad was loaded');};
+                                               successLoad.onload = function() {console.log('ad');};
                                                successLoad.open("GET", adsData.session.beacons.inbox_open, true);
-                                               successLoad.send();
+                                               //successLoad.send();
+                                           };
+                                           document.getElementsByClassName('like')[0].onclick = function() {
+                                               var like = new XMLHttpRequest();
+                                               like.open("GET", adsData.ads[0].beacons.ad_like, true);
+                                               like.send();
+                                           };
+                                           document.getElementsByClassName('dislike')[0].onclick = function() {
+                                               //document.getElementsByClassName('button').className  = "button_click";
+                                           };
+                                           document.getElementsByClassName('stop')[0].onclick = function() {
+                                               var stop = new XMLHttpRequest();
+                                               stop.open("GET", adsData.ads[0].beacons.ad_hide, true);
+                                               stop.send();
+                                           };
+                                           document.getElementsByClassName('share')[0].onclick = function() {
+                                               var share = new XMLHttpRequest();
+                                               share.open("GET", adsData.ads[0].beacons.ad_share, true);
+                                               share.send();
                                            };
                                        }
                                        else {
